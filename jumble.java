5acdf4895b6ad891ad6c020e88f11120d18be097
yokeshwaranb <41099344+yokeshwaranb@users.noreply.github.com>
2018-07-12 09:48:34 +0530

    Add files via upload

---
+++ jumble.java
@@ -0,0 +1,14 @@
+package yokesh;
+import java.util.*;
+import java.lang.*;
+public class jumble {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str,temp;
+		Scanner in=new Scanner(System.in);
+		str=in.nextLine();
+		
+	}
+
+}
